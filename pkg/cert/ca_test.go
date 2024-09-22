e51c86a7e134deea029fdb9f1624e9c9d5f082e7
Luke Griffith <lukemgriffith@gmail.com>
2024-09-22 12:13:44 +0100

    updating models and core functions

--- pkg/cert/ca_test.go
+++ pkg/cert/ca_test.go
@@ -8,25 +8,25 @@ func TestCaValidation(t *testing.T) {
 		res bool
 	}{
 		// Test cases
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 2048}, true},          // Valid case
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 3072}, true},          // Valid case
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 4096}, true},          // Valid case
-		{CaRequest{Name: "TestCA", Type: "ED25519", Bits: 2048}, false},     // Invalid case (wrong Type)
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 1024}, false},         // Invalid case (wrong Bits)
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 8192}, false},         // Invalid case (unsupported Bits)
-		{CaRequest{Name: "", Type: "rsa", Bits: 2048}, false},               // Invalid case (missing Name)
-		{CaRequest{Name: "TestCA", Type: "InvalidType", Bits: 2048}, false}, // Invalid case (wrong Type)
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 1234}, false},         // Invalid case (wrong Bits)
-		{CaRequest{Name: "TestCA", Type: "", Bits: 2048}, false},            // Invalid case (empty Type)
-		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 0}, false},            // Invalid case (Bits = 0)
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 2048, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, true},          // Valid case
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 3072, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, true},          // Valid case
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 4096, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, true},          // Valid case
+		{CaRequest{Name: "TestCA", Type: "ED25519", Bits: 2048, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},     // Invalid case (wrong Type)
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 1024, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},         // Invalid case (wrong Bits)
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 8192, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},         // Invalid case (unsupported Bits)
+		{CaRequest{Name: "", Type: "rsa", Bits: 2048, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},               // Invalid case (missing Name)
+		{CaRequest{Name: "TestCA", Type: "InvalidType", Bits: 2048, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false}, // Invalid case (wrong Type)
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 1234, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},         // Invalid case (wrong Bits)
+		{CaRequest{Name: "TestCA", Type: "", Bits: 2048, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},            // Invalid case (empty Type)
+		{CaRequest{Name: "TestCA", Type: "rsa", Bits: 0, ValidPrincipals: []string{"testuser"}, MaxTTLMinutes: 3600}, false},            // Invalid case (Bits = 0)
 	}
 
 	// Loop through test cases
 	for _, tt := range table {
 		t.Run(tt.ca.Name, func(t *testing.T) {
-			result := tt.ca.Validate()
+			err, result := tt.ca.Validate()
 			if result != tt.res {
-				t.Errorf("expected %v, got %v", tt.res, result)
+				t.Errorf("expected %v, got %v, %s", tt.res, result, err)
 			}
 		})
 	}
