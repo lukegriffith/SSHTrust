e33138b91e29ef5037017d297969312406b24576
Luke Griffith <lukemgriffith@gmail.com>
2024-10-16 21:48:37 +0100

    adding

--- pkg/auth/acl.go
+++ pkg/auth/acl.go
@@ -1,16 +1,23 @@
 package auth
 
 type ACL struct {
-	CA        string
+	CA         string
 	Principals []string
 	Permission bool
 }
 
-var _ = ACL{
-	CA: "TestCA".
-	Principals: []string{
-		"group/test",
-		"user/admin",
-	}
-	Permission: true,
+var _ = []ACL{
+	ACL{
+		CA:         "*",
+		Principals: []string{},
+		Permission: false,
+	},
+	ACL{
+		CA: "TestCA",
+		Principals: []string{
+			"group/test",
+			"user/admin",
+		},
+		Permission: true,
+	},
 }
